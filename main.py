7940a9c268ce0b2971c055ea16bbaa671037bdec
Alan <jihong620@gmail.com>
2025-03-22 17:48:29 +0800

    feat 新增功能

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # main.py
 def main():
     print("Git 教學")
+<<<<<<< HEAD
     print("Hank 已經解除 bug 了")
 
+=======
+    print("Alan 已經新增功能了")
+    
+>>>>>>> 76686e8 (feat 新增功能)
 if __name__ == "__main__":
     main()
